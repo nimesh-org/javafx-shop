9e7066fec157261a805095df444d9730f4b21b58
Nimesh175 <nimeshdwejerathna2020.com>
2021-08-07 12:33:50 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This isjava fx application to save customers and items to the database
---
+++ src/lk/ijse/shop/controller/Controller.java
@@ -0,0 +1,15 @@
+/*
+ *  ---------------------------------------------------------------------------------------------
+ *  Copyright (c) 2021 - present IJSE. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *  --------------------------------------------------------------------------------------------
+ */
+
+package lk.ijse.shop.controller;
+/**
+ * @author Dinuka Nimesh <nimeshdwejerathna2020@gmail.com>
+ * @since {8/7/2021}
+ */
+
+public class Controller {
+}
